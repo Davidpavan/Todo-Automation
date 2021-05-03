fdebcee8d864dc7161275733fe65b0c5a5b9b10e
Davidpavan <David@zelarsoft.com>
2021-05-03 19:14:14 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source components/common.sh
+
 Head "Installing golang"
 
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
 
-#Head "Adjusting the Path Variable"
+Head "Adjusting the Path Variable"
 
 export PATH=$PATH:/usr/local/go/bin && source ~/.profile && go version &>>$LOG
 
