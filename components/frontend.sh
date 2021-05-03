f8d31e10534eb42c8dc320caaab0e4ba1539d83b
Davidpavan <David@zelarsoft.com>
2021-05-03 16:20:23 +0530

    Updating frontend

---
+++ components/frontend.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source components/common.sh
+
+OS_PREREQ
+
+Head "Installing Nginx"
+apt install nginx -y &>>$LOG
+Stat $?
+
+Head "Installing Nodejs"
+apt install nodejs -y &>>$LOG
+Stat $1
+
+Head "Installing npm"
+apt install npm -y &>>$LOG
+Stat $1
+
+DOWNLOAD_COPONENT
+
+Head "Clonning Git Repo & Installing npm depedencies"
+
+git clone https://github.com/zelar-soft-todoapp/frontend.git && cd frontend && npm install && npm run build
+Stat $1
+
+Head "Restart Nginx service"
+systemctl nginx restart
+Stat $1
+
+Head "Starting npm"
+npm start
+Stat $1
