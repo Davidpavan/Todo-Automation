07633246947fd323007ea620323ce8b40d9705eb
Davidpavan <David@zelarsoft.com>
2021-05-05 12:10:52 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -5,12 +5,13 @@ source components/common.sh
 apt update &>>$LOG
 
 Head "Installing login dependencies"
-
-apt install golang -y &>>$LOG
+cd
+wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
+export PATH=$PATH:/usr/local/go/bin
+source ~/.profile
 Stat $?
 
 Head "Cloning Repo"
-cd
 mkdir /go && cd /go && mkdir src && cd src
 git clone https://github.com/Davidpavan/login.git &>>$LOG
 Stat $?
@@ -26,12 +27,6 @@ Stat $?
 Head "Creating Service"
 
 mv /go/src/login/login.service /etc/systemd/system/login.service
-Stat $?
-
-Head "Get dependencies"
-if [ $? -ne 0 ]; then
- go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
-fi
 Stat $?
 
 Head "Building Package"
