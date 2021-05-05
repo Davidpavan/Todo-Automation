cede733e8d25508cebba8e0d13912bef485dc755
Davidpavan <David@zelarsoft.com>
2021-05-05 12:29:02 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -11,14 +11,21 @@ export PATH=$PATH:/usr/local/go/bin
 source ~/.profile
 Stat $?
 
+Head "Getting dependencies"
+if [ $? -ne 0 ]; then
+ go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
+fi
+Stat $?
+
 Head "Cloning Repo"
 mkdir /go && cd /go && mkdir src && cd src
 git clone https://github.com/Davidpavan/login.git &>>$LOG
 Stat $?
 
-Head "Navigate Directory"
-
-mv Todo-login login &>>$LOG && cd login && rm -rf login &>>$LOG
+Head "Building Package"
+cd login && export GOPATH=/go && go get
+go build &>>$LOG
+Stat $?
 
 Head "Update EndPoints in Service File"
 sed -i -e "s/USERS_DNSNAME/users.pavanzs.online/" /go/src/login/login.service
@@ -27,11 +34,6 @@ Stat $?
 Head "Creating Service"
 
 mv /go/src/login/login.service /etc/systemd/system/login.service
-Stat $?
-
-Head "Building Package"
-cd /go  && cd src && cd login && export GOPATH=/go && go get
-go build &>>$LOG
 Stat $?
 
 Head "Restarting Services"
