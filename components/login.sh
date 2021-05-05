36c786b427145c8e2b6294737c9c4225c17d76cd
Davidpavan <David@zelarsoft.com>
2021-05-05 10:44:09 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -19,22 +19,22 @@ Head "Navigate Directory"
 
 mv Todo-login login &>>$LOG && cd login && rm -rf login &>>$LOG
 
-Head "Creating Service"
+Head "Update EndPoints in Service File"
+sed -i -e "s/USERS_DNSNAME/users.pavanzs.online/" /go/src/login/login.service
+Stat $?
 
+Head "Creating Service"
 mv /go/src/login/login.service /etc/systemd/system/login.service
 
 Head "Get dependencies"
-
 go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
 Stat $?
 
 Head "Building Package"
-
 cd /go && cd src && cd login
 go build &>>$LOG
 Stat $?
 
 Head "Restarting Services"
-
 systemctl daemon-reload &>>$LOG && systemctl start login && systemctl enable login &>>$LOG
 Stat $?
