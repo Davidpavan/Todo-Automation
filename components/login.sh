7d2b59b9132917d676b41de14c3623c23dea4035
Davidpavan <David@zelarsoft.com>
2021-05-05 12:31:47 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -6,12 +6,6 @@ apt update &>>$LOG
 
 Head "Installing login dependencies"
 cd
-wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
-export PATH=$PATH:/usr/local/go/bin
-source ~/.profile
-Stat $?
-
-Head "Getting dependencies"
 if [ $? -ne 0 ]; then
  go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
 fi
