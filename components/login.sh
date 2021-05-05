6a4b42d7530647f197f3bf4353974092b0948532
Davidpavan <David@zelarsoft.com>
2021-05-05 13:07:29 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -6,7 +6,6 @@ OS_PREREQ
 
 head "Installing golang"
 
-cd /root
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
 export PATH=$PATH:/usr/local/go/bin
 source ~/.profile
