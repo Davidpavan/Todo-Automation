ead7b8a381b136506645bf69650316f5b7cef891
Davidpavan <David@zelarsoft.com>
2021-05-05 13:20:32 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -4,31 +4,29 @@ source components/common.sh
 
 OS_PREREQ
 
-head "Installing golang"
-
+Head "Installing golang"
+cd /root
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
 export PATH=$PATH:/usr/local/go/bin
 source ~/.profile
 Stat $?
-
-head "version of golang"
+Head "version of golang"
 go version
 Stat $?
-head "To create a directory"
+Head "To create a directory"
     mkdir /go && cd /go
-
-head "To clone the git Repo in to login"
+Head "To clone the git Repo in to login"
 git clone https://github.com/Davidpavan/login.git &>>$LOG
 cd login && export GOPATH=/go
 apt install go-dep &>>$LOG
 Stat $?
-head "to build the golang"
+Head "to build the golang"
 go get
 go build
 Stat $?
-head "To create a systemd file"
+Head "To create a systemd file"
  mv login.service /etc/systemd/system/login.service
-head "To start a service"
+Head "To start a service"
 systemctl daemon-reload &&
 service login start &&
 service login restart &&
