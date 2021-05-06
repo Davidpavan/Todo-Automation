1d5801101b1ef553825ca0f9c76cd8ec78f89586
Davidpavan <David@zelarsoft.com>
2021-05-06 13:27:00 +0530

    Updating Shell-scripting/

--- components/users.sh
+++ components/users.sh
@@ -22,7 +22,7 @@ Stat $?
 
 Head "Updating service"
 
-mv /root/users/users.service /etc/systemd/system/users.service &>>$LOG
+mv /root/Todo-Automation/users/users.service /etc/systemd/system/users.service &>>$LOG
 
 Head "Starting Service"
 
