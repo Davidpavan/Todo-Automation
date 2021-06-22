aaf9bbdba0cb6bb83db1184618e88bcf2f8ec741
Davidpavan <David@zelarsoft.com>
2021-06-23 00:46:02 +0530

    1.First Commit

--- components/todo.sh
+++ components/todo.sh
@@ -15,7 +15,7 @@ npm install &>>$LOG
 Stat $?
 
 Head "Update EndPoints in Service File"
-sed -i -e "s/REDIS_DNSNAME/192.168.0.147/" /root/todo/todo.service &>>$LOG
+sed -i -e "s/REDIS_DNSNAME/192.168.0.136/" /root/todo/todo.service &>>$LOG
 
 Head "Moving Servicefile"
 mv /root/todo/todo.service /etc/systemd/system/todo.service &>>$LOG
