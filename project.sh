551330fb074d65e74f970a215e0e1b252cfbd11b
Davidpavan <David@zelarsoft.com>
2021-07-19 23:14:20 +0530

    Updating Shell-scripting/

--- project.sh
+++ project.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-ssh ubuntu@172.31.10.169 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh redi"
+ssh ubuntu@redis.pavanzs.online "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh redis"
 
-ssh ubuntu@172.31.13.243 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh todo"
+ssh ubuntu@todo.pavanzs.online "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh todo"
 
-ssh ubuntu@172.31.9.83 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh users"
+ssh ubuntu@users.pavanzs.online "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh users"
 
-ssh ubuntu@172.31.2.35 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh login"
+ssh ubuntu@login.pavanzs.online "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh login"
 
-ssh ubuntu@172.31.4.190 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh frontend"
+ssh ubuntu@frontend.pavanzs.online "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-automation ; sudo bash Todo.sh frontend"
