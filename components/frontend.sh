fd52d2f9cef6340704380ab2db2e7d34762c84bb
Davidpavan <David@zelarsoft.com>
2021-07-25 19:16:36 +0530

    Updating Shell-scripting/

--- components/frontend.sh
+++ components/frontend.sh
@@ -35,7 +35,7 @@ mv todo.conf /etc/nginx/sites-enabled/todo.conf
 Stat $?
 
 Head "Update EndPoints in Conf File"
-sed -i -e "s/127.0.0.1/192.168.0.76/" /etc/nginx/sites-enabled/todo.conf && sed -i -e "s/127.0.0.2/192.168.0.11/" /etc/nginx/sites-enabled/todo.conf
+sed -i -e "s/127.0.0.1/172.31.10.213/" /etc/nginx/sites-enabled/todo.conf && sed -i -e "s/127.0.0.2/172.31.12.236/" /etc/nginx/sites-enabled/todo.conf
 Stat $?
 
 
