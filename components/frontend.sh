fa31ccfc93a188404ac00a7a2ccf382eba77925f
Davidpavan <David@zelarsoft.com>
2021-07-25 22:23:27 +0530

    Updating Shell-scripting/

--- components/frontend.sh
+++ components/frontend.sh
@@ -19,7 +19,7 @@ Stat $?
 
 Head "Remove Default Configuration"
 rm -rf /var/www/html/* /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
-cd /var/www/html
+cd /var/www/html/
 Stat $?
 
 DOWNLOAD_COMPONENT
