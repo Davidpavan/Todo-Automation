aae6a5d7e828f189a6a42608909718e04e6acc1f
Davidpavan <David@zelarsoft.com>
2021-07-26 17:33:05 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
+
 source components/common.sh
+
 OS_PREREQ
-head "Installing golang"
-cd /root
+
+Head " Installing golang"
+cd
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
-export PATH=$PATH:/usr/local/go/bin
-source ~/.profile
 Stat $?
-head "version of golang"
-go version
+
+Head " Adjusting path variables"
+export PATH=$PATH:/usr/local/go/bin &>>$LOG
+source ~/.profile &>>$LOG
+go version &>>$LOG
 Stat $?
-head "To create a directory"
+
+Head " Creating a new directory"
 mkdir /go && cd /go
-head "To clone the git Repo in to login"
-git clone https://github.com/Davidpavan/login.git &>>$LOG
-cd login && export GOPATH=/go
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+Head " Build the Source-code"
+cd login && export GOPATH=~/go &>>$LOG
 apt install go-dep &>>$LOG
+go get &>>$LOG && go build &>>$LOG
 Stat $?
-head "to build the golang"
-go get
-go build
+
+Head "Creating Service"
+cp /root/go/src/login/login.service /etc/systemd/system/login.service &>>$LOG
 Stat $?
-head "To create a systemd file"
-mv login.service /etc/systemd/system/login.service &>>$LOG
-head "Update EndPoints in Service File"
-sed -i -e "s/localhost/192.168.0.65/" /etc/systemd/system/login.service
+
+Head "Update EndPoints in Service File"
+sed -i -e "s/USERS_DNSNAME/192.168.0.65/" /root/go/src/login/login.service &>>$LOG
 Stat $?
-head "To start a service"
-systemctl daemon-reload &&
-service login start &&
-service login restart &&
-service login status
+
+
+Head "starting service"
+systemctl daemon-reload && systemctl enable login &>>$LOG && systemctl start login && systemctl restart login
 Stat $?
