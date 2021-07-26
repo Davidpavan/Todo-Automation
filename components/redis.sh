1af78d74d80d0f78b4dd4d025a505af0a62b7f62
Davidpavan <David@zelarsoft.com>
2021-07-26 16:47:55 +0530

    Updating Shell-scripting/

--- components/redis.sh
+++ components/redis.sh
@@ -5,13 +5,13 @@ source components/common.sh
 OS_PREREQ
 
 Head "Install Redis"
-sudo apt install redis-server -y &>>$LOG
+apt install redis-server -y &>>$LOG
 Stat $?
 
 Head "Update Redis Listen Address"
-sudo sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 Stat $?
 
 Head "Start Redis Service"
-sudo systemctl start redis
+systemctl start redis
 Stat $?
