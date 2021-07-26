c7cf9e956fa81688410a75b1c1dfb053cea54c2d
Davidpavan <David@zelarsoft.com>
2021-07-26 17:59:46 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -21,14 +21,14 @@ apt install go-dep &>>$LOG
 go get && go build &>>$LOG
 Stat $?
 
+Head "Update EndPoints in Service File"
+sed -i -e "s/USERS_DNSNAME/192.168.0.65/" /root/go/src/login/login.service &>>$LOG
+Stat $?
+
 Head "Creating Service"
 cp /root/go/src/login/login.service /etc/systemd/system/login.service &>>$LOG
 Stat $?
 
-Head "Update EndPoints in Service File"
-
-sed -i -e "s/USERS_DNSNAME/192.168.0.65/" /root/go/src/login/login.service &>>$LOG
-Stat $?
 
 Head "starting service"
 systemctl daemon-reload && systemctl enable login &>>$LOG && systemctl start login && systemctl restart login
