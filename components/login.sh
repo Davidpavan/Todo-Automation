9f7f05c7954d10d1681570dad9bfd28c6853adb6
Davidpavan <David@zelarsoft.com>
2021-07-26 17:36:45 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -35,7 +35,6 @@ Head "Update EndPoints in Service File"
 sed -i -e "s/USERS_DNSNAME/192.168.0.65/" /root/go/src/login/login.service &>>$LOG
 Stat $?
 
-
 Head "starting service"
 systemctl daemon-reload && systemctl enable login &>>$LOG && systemctl start login && systemctl restart login
 Stat $?
