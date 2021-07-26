254a0ceea0ffa5ff9ffdc2a61d52969c2dd58a31
Davidpavan <David@zelarsoft.com>
2021-07-26 14:37:30 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -15,7 +15,7 @@ go version &>>$LOG
 Stat $?
 
 Head " Creating a new directory"
-mkdir -p ~/go && cd ~/go && mkdir -p src && cd src &>>$LOG
+mkdir -p ~/go/src && cd src &>>$LOG
 Stat $?
 
 DOWNLOAD_COMPONENT
