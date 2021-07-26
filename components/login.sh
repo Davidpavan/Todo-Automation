7f22755d7b6dd6244c26e5b4087c924eb8fde19a
Davidpavan <David@zelarsoft.com>
2021-07-26 17:20:27 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -16,10 +16,12 @@ Stat $?
 
 Head " Creating a new directory"
 mkdir -p ~/go && cd ~/go
-if [ -d "$src" ]; then
-  cd src && DOWNLOAD_COMPONENT
+if [ -d "src" ]; then
+  cd src
 fi
 Stat $?
+
+DOWNLOAD_COMPONENT
 
 Head " Build the Source-code"
 export GOPATH=~/go &>>$LOG
