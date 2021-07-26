908a345de7a2f6f165fb0f81afbff2d72229aa54
Davidpavan <David@zelarsoft.com>
2021-07-26 17:40:45 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -22,7 +22,7 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head " Build the Source-code"
-cd login && export GOPATH=~/go &>>$LOG
+cd login && export GOPATH=/go &>>$LOG
 apt install go-dep &>>$LOG
 go get &>>$LOG && go build &>>$LOG
 Stat $?
