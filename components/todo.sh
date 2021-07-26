8f2d963c5ddc0c4f4a316e05166c6dbba86f9675
Davidpavan <David@zelarsoft.com>
2021-07-26 16:45:50 +0530

    Updating Shell-scripting/

--- components/todo.sh
+++ components/todo.sh
@@ -6,21 +6,21 @@ OS_PREREQ
 
 Head "Install NPM"
 cd
-sudo apt install npm -y &>>$LOG
+apt install npm -y &>>$LOG
 Stat $?
 
 DOWNLOAD_COMPONENT
 
 Head "Download Dependencies"
-sudo npm install &>>$LOG
+npm install &>>$LOG
 Stat $?
 
 Head "Update EndPoints in Service File"
-sudo sed -i -e "s/REDIS_DNSNAME/192.168.0.110/" /root/todo/todo.service &>>$LOG
+sed -i -e "s/REDIS_DNSNAME/192.168.0.110/" /root/todo/todo.service &>>$LOG
 
 Head "Moving Servicefile"
-sudo cp /root/todo/todo.service /etc/systemd/system/todo.service &>>$LOG
+cp /root/todo/todo.service /etc/systemd/system/todo.service &>>$LOG
 
 Head "Starting Service"
-sudo systemctl daemon-reload && sudo systemctl enable todo &>>$LOG && sudo systemctl start todo
+systemctl daemon-reload && systemctl enable todo &>>$LOG && systemctl start todo
 Stat $?
