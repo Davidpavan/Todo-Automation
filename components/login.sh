f98b4aae0243b8404550ddff2da53f2007974a91
Davidpavan <David@zelarsoft.com>
2021-07-26 19:38:04 +0530

    Updating Shell-scripting/

--- components/login.sh
+++ components/login.sh
@@ -12,7 +12,7 @@ Stat $?
 Head " Creating a new directory"
 if [! -d "~/go"]
 then
-  mkdir ~/go && cd ~/go && mkdir src && cd src &>>$LOG
+  mkdir ~/go && cd ~/go && mkdir src && cd src
 else if [ -d "~/go"]
 then
   cd ~/go/src
