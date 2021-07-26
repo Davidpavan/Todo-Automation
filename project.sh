742cb4df2dc83a15a90887ee1041fcbc601a75b7
Davidpavan <David@zelarsoft.com>
2021-07-26 13:53:39 +0530

    Updating Shell-scripting/

--- project.sh
+++ project.sh
@@ -2,7 +2,7 @@
 
 sshpass -p 'DevOps321' ssh ubuntu@192.168.0.38 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh frontend"
 
-sshpass -p 'DevOps321' ssh ubuntu@192.168.0.91 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh login"
+sshpass -p 'DevOps321' ssh ubuntu@192.168.0.120 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh login"
 
 sshpass -p 'DevOps321' ssh ubuntu@192.168.0.67 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh users"
 
