44693519a5d1b08a10f3973da770c71439040466
Davidpavan <David@zelarsoft.com>
2021-07-27 15:20:00 +0530

    Updating Shell-scripting/

--- project.sh
+++ project.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-sshpass -p 'DevOps321' ssh ubuntu@192.168.0.58 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh frontend"
+sshpass -p 'DevOps321' ssh ubuntu@3.89.10.161 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh frontend"
 
-sshpass -p 'DevOps321' ssh ubuntu@192.168.0.113 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh login"
+sshpass -p 'DevOps321' ssh ubuntu@172.31.91.124 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh login"
 
-sshpass -p 'DevOps321' ssh ubuntu@192.168.0.79 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh users"
+sshpass -p 'DevOps321' ssh ubuntu@172.31.86.128 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh users"
 
-sshpass -p 'DevOps321' ssh ubuntu@192.168.0.9 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh todo"
+sshpass -p 'DevOps321' ssh ubuntu@172.31.90.38 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh todo"
 
-sshpass -p 'DevOps321' ssh ubuntu@192.168.0.87 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh redis"
+sshpass -p 'DevOps321' ssh ubuntu@172.31.93.77 "git clone https://github.com/Davidpavan/Todo-Automation.git ; cd Todo-Automation ; sudo bash Todo.sh redis"
