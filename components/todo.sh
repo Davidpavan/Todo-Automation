60ae93d685cfcd0cb8bfff761b4d6b641243f90f
Davidpavan <David@zelarsoft.com>
2021-07-28 12:49:01 +0530

    Updating Shell-scripting/

--- components/todo.sh
+++ components/todo.sh
@@ -16,7 +16,7 @@ npm install &>>$LOG
 Stat $?
 
 Head "Update EndPoints in Service File"
-sed -i -e "s/REDIS_DNSNAME/172.31.93.77/" /root/todo/todo.service &>>$LOG
+sed -i -e "s/REDIS_DNSNAME/192.168.0.87/" /root/todo/todo.service &>>$LOG
 
 Head "Moving Servicefile"
 cp /root/todo/todo.service /etc/systemd/system/todo.service &>>$LOG
